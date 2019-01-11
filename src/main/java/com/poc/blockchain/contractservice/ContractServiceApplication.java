9c0c70888c58f26245101b7355de1b82225d04ed
HarishSekar <hasekar@ciber.com>
2019-01-11 15:07:12 -0500

    Initial commit

---
+++ src/main/java/com/poc/blockchain/contractservice/ContractServiceApplication.java
@@ -0,0 +1,14 @@
+package com.poc.blockchain.contractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractServiceApplication.class, args);
+    }
+
+}
+
